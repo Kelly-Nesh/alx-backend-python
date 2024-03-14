7aebb8cd356a6913a35b675b0c1605d83b1ba7d8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-14 22:48:38 +0300

    function documentation for 8 & 9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """type-annotated function that takes a float multiplier as argument
+    and returns a function that multiplies a float by the multiplier"""
     def callable(arg: float):
         return arg * multiplier
     return callable
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,7 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    type-annotated function that takes an iterable of sequences
+    and returns a list of tuples of the sequence and its length"""
     return [(i, len(i)) for i in lst]
