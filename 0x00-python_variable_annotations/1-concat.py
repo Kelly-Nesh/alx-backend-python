f01e62ac501efa392e5a6c43833b61cf45706c1b
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-14 16:45:15 +0300

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type-annotated function that takes two strings
+    returns their sum as a string."""
+    return str1 + str2
