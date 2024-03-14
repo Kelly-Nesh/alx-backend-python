00c205eaea7dd1513578455c44ff217868ceb4e9
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-14 20:34:17 +0300

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """type-annotated function that takes a float
+    returns its integer value."""
+    return math.floor(n)
