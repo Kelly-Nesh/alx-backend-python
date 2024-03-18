ee5d9546c0f0d6467a438b20ccb0a9b2a069c118
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-19 02:19:00 +0300

    wrong type annotation for function params fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,14 +2,13 @@
 """The basics of async"""
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Takes in an integer argument (max_delay, with a default value of 10)
     waits for a random delay between 0 and
         max_delay (included and float value) seconds
     returns delay time."""
-    delay: float = random.uniform(0, max_delay)
+    delay: float = random.uniform(0.0, max_delay)
     await asyncio.sleep(delay)
     return delay
