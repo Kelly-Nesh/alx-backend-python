c82aa22f85ea2c702c9158016dd97fcd4e54da1c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-19 02:54:54 +0300

    pycodestyle formatting compliance

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Measure the runtime"""
-wait_n = __import__("1-concurrent_coroutines").wait_n
-import asyncio
 import time
+import asyncio
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the runtime of wait_n(n, max_delay)"""
