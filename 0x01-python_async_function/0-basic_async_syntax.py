3413b71bd2c34509d4a85edbb3cc6c142ab7bfb3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-19 02:04:54 +0300

    type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,13 +2,14 @@
 """The basics of async"""
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: Union[int, float] = 10) -> Union[int, float]:
     """Takes in an integer argument (max_delay, with a default value of 10)
     waits for a random delay between 0 and
         max_delay (included and float value) seconds
     returns delay time."""
-    delay = random.uniform(0, max_delay)
+    delay: Union[int, float] = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
