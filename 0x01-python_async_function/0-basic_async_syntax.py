2ed81771276c8dc205a85d052b257e63aaa77b67
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-19 02:07:18 +0300

    fixed wrong type annotation for return value made file executable

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,11 +5,11 @@ import random
 from typing import Union
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> Union[int, float]:
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
     """Takes in an integer argument (max_delay, with a default value of 10)
     waits for a random delay between 0 and
         max_delay (included and float value) seconds
     returns delay time."""
-    delay: Union[int, float] = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
