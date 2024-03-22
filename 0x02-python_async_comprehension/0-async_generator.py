4fcd9a6c4ad864a221c5429482234a183fd5170f
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-22 15:05:35 +0300

    changed return type to asyncgenerator with float,none

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, int]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     An asynchronous generator that yields random numbers between 0 and 10
     after a 1 second delay.
